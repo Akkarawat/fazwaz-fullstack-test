1c84425b4e04c203dab7e7de5861cc77abd9d8ee
Akkharawat <chambangrak@gmail.com>
2025-03-09 20:38:17 +0700

    fix: improving db seeder to give more diverse created_at and updated_at

--- html/backend/database/seeders/PropertySeeder.php
+++ html/backend/database/seeders/PropertySeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Property;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\File;
 
 class PropertySeeder extends Seeder
@@ -13,7 +14,12 @@ class PropertySeeder extends Seeder
         $json = File::get(database_path('seeders/data/properties.json'));
         $properties = json_decode($json, true);
 
+        $startDate = Carbon::parse('2000-01-01 00:00:00');
+        $endDate = Carbon::parse('2025-03-01 23:59:59');
+
         foreach ($properties as $property) {
+            $randomeTime = $this->generateRandomDateTime($startDate, $endDate);
+
             Property::create([
                 'id' => $property['id'],
                 'title' => $property['title'],
@@ -35,9 +41,19 @@ class PropertySeeder extends Seeder
                 'photos_thumb' => $property['photos']['thumb'],
                 'photos_search' => $property['photos']['search'],
                 'photos_full' => $property['photos']['full'],
-                'created_at' => now(),
-                'updated_at' => now(),
+                'created_at' => $randomeTime,
+                'updated_at' => $randomeTime,
             ]);
         }
+    }
+
+    private function generateRandomDateTime(Carbon $startDate, Carbon $endDate)
+    {
+        $startTimestamp = $startDate->timestamp;
+        $endTimestamp = $endDate->timestamp;
+
+        $randomTimestamp = mt_rand($startTimestamp, $endTimestamp);
+
+        return Carbon::createFromTimestamp($randomTimestamp);
     }
 }
