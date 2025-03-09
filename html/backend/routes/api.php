3d7920498a01e8d3c430078ebfe25f89ee68ca9a
Akkharawat <chambangrak@gmail.com>
2025-03-09 12:58:43 +0700

    feat: returning error as json

--- html/backend/routes/api.php
+++ html/backend/routes/api.php
@@ -1,8 +1,16 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PropertyController;
 
 Route::prefix('/properties')->group(function () {
     Route::get('/', [PropertyController::class, 'getProperties']);
+});
+
+Route::fallback(function (Request $request) {
+    return response()->json([
+        'exception' => 'Not Found',
+        'message' => 'This API route does not exist.',
+    ], 404);
 });
